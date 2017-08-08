bdbc2b9920d5422ecf159cc228b80492ed5da3a3
HJRaBob <30286304+HJRaBob@users.noreply.github.com>
2017-08-08 14:04:15 +0900

    Add files via upload

---
+++ _02_Hello_Arduino.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //통신 속도 1초에: 9600개의 데이터를 보내겠다
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello Arduion"); //보드에서 컴퓨터로 데이터를 보냄(tx:trasport 가 반짝임)
+  delay(1000);
+}
