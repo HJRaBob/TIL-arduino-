6a0056db4ab5eaa3d626696c6a5b7dfeec5b4359
HJRaBob <30286304+HJRaBob@users.noreply.github.com>
2017-08-08 14:04:57 +0900

    Add files via upload

---
+++ _03_Trapic_light.ino
@@ -0,0 +1,51 @@
+int REDpin = 6;
+int YELLOWpin = 4;
+int GREENpin = 5;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(REDpin,OUTPUT);
+  pinMode(YELLOWpin,OUTPUT);
+  pinMode(GREENpin,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(REDpin,HIGH);
+  delay(1000);
+  digitalWrite(REDpin,LOW);
+  digitalWrite(YELLOWpin,HIGH);
+  delay(1000);
+  digitalWrite(YELLOWpin,LOW);
+  digitalWrite(GREENpin,HIGH);
+  delay(1000);
+  digitalWrite(GREENpin,LOW);
+
+  delay(1000);
+
+  digitalWrite(REDpin,HIGH);
+  digitalWrite(YELLOWpin,HIGH);
+  digitalWrite(GREENpin,HIGH);
+  delay(1000);
+  digitalWrite(REDpin,LOW);
+  digitalWrite(YELLOWpin,LOW);
+  digitalWrite(GREENpin,LOW);
+  delay(1000);
+  digitalWrite(REDpin,HIGH);
+  digitalWrite(YELLOWpin,HIGH);
+  digitalWrite(GREENpin,HIGH);
+  delay(1000);
+  digitalWrite(REDpin,LOW);
+  digitalWrite(YELLOWpin,LOW);
+  digitalWrite(GREENpin,LOW);
+  delay(1000);
+  digitalWrite(REDpin,HIGH);
+  digitalWrite(YELLOWpin,HIGH);
+  digitalWrite(GREENpin,HIGH);
+  delay(1000);
+  digitalWrite(REDpin,LOW);
+  digitalWrite(YELLOWpin,LOW);
+  digitalWrite(GREENpin,LOW);
+  delay(1000);
+}
