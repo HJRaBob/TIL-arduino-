90a3d4ff0f2e45b5202a9484ac83466a884fbb8f
HJRaBob <30286304+HJRaBob@users.noreply.github.com>
2017-08-08 14:05:28 +0900

    Add files via upload

---
+++ _04_02_switch_bounce.ino
@@ -0,0 +1,20 @@
+bool flag = 0;
+void setup() {
+  // put your setup code here, to run once:
+ // Serial.begin(9600);
+  pinMode(6,OUTPUT);
+  pinMode(8,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = digitalRead(8);
+  
+  if(val == HIGH)
+  {
+    flag = !flag;
+    delay(200);
+  }
+  digitalWrite(6,flag%2);
+ // Serial.println(flag);
+}
