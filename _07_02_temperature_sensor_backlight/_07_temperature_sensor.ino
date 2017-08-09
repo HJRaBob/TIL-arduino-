0b9fd5bba632cda0629143ee53db53528b3420a1
HJRaBob <30286304+HJRaBob@users.noreply.github.com>
2017-08-09 16:03:52 +0900

    Add files via upload

---
+++ _07_02_temperature_sensor_backlight/_07_temperature_sensor.ino
@@ -0,0 +1,30 @@
+#include<LiquidCrystal.h>
+
+LiquidCrystal lcd(12,11,5,4,3,2);
+int temp;
+int lux;
+int i;
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.begin(16,2);
+  pinMode(9,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lux = analogRead(0); //bright
+  temp = analogRead(1); //temperature
+  delay(1000);
+  analogWrite(9,i); //backlight bright
+  i+=10;
+  if (i>255){
+    i=0;
+  }
+  lcd.setCursor(0,0);
+  lcd.print("brite: ");
+  lcd.print(lux);
+  lcd.setCursor(0,1);
+  lcd.print("temper: ");
+  lcd.print((5.0*temp*100.0)/1024.0);
+}
