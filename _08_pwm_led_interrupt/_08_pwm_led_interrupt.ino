472d8be8f2ece51d2513b4e9e0936c6f70bb4040
HJRaBob <30286304+HJRaBob@users.noreply.github.com>
2017-08-11 08:40:39 +0900

    Add files via upload

---
+++ _08_pwm_led_interrupt/_08_pwm_led_interrupt.ino
@@ -0,0 +1,53 @@
+bool flag_1 = false;
+bool flag_2 = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  attachInterrupt(0,stop1,FALLING);
+  attachInterrupt(1,stop2,FALLING);
+}
+
+void stop1()
+{
+  delay(200);
+  flag_1 = !flag_1;
+}
+
+void stop2()
+{
+  delay(200);
+  flag_2 = !flag_2;
+}
+
+void loop() {
+    for(int i=0; i<256; i++)
+    {
+      if(!flag_1){
+        analogWrite(9, i);
+      }else{
+        analogWrite(9, 0);
+      }
+      if(!flag_2){
+        analogWrite(10, i);
+      }else{
+        analogWrite(10, 0);
+      }
+      delay(10);
+    }
+    for(int i=255; i>=0; i--)
+    {
+       if(!flag_1){
+        analogWrite(9, i);
+        delay(10);
+      }else{
+        analogWrite(9, 0);
+      }
+      if(!flag_2){
+        analogWrite(10, i);
+      }else{
+        analogWrite(10, 0);
+      }
+    }
+}
