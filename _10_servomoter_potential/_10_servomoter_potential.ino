92aba7613005e456d86ec6bd7cec4128fa97ac66
HJRaBob <30286304+HJRaBob@users.noreply.github.com>
2017-08-12 12:23:43 +0900

    Add files via upload

---
+++ _10_servomoter_potential/_10_servomoter_potential.ino
@@ -0,0 +1,21 @@
+#include<Servo.h>
+Servo servo;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  servo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(A0);
+  Serial.print("potentialmeter :");
+  Serial.println(val);
+
+  val = map(val,0,1023,0,179);
+  servo.write(val);
+  Serial.print("angle is");
+  Serial.println(val);
+  delay(200);
+}
